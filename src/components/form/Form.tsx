10ba6326a3de23db41bf11fa22b729ac372389c9
bekatru <teamusicband@gmail.com>
2023-12-12 16:49:23 +0600

    refactor Form.tsx to use MUI components

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,22 +1,41 @@
 import {FC, FormEvent, useCallback} from 'react';
 import './Form.css';
+import {Button, TextField, ThemeProvider} from '@mui/material';
+import {Send} from '@mui/icons-material';
+import {createTheme} from '@mui/material/styles';
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 
 const Form: FC = () => {
 
   const handleSubmit = useCallback((e: FormEvent) => {
     e.preventDefault();
-    console.log(e)
-  }, [])
+    console.log(e);
+  }, []);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type={'text'}/>
-      <input type={'email'}/>
-      <textarea/>
-      <button type={'submit'}>send</button>
-    </form>
-  )
+    <ThemeProvider theme={darkTheme}>
+      <form onSubmit={handleSubmit}>
+        <TextField id="standard-basic" label="name" variant="standard" margin={'dense'}/>
+        <TextField id="standard-basic" label="email" variant="standard" margin={'dense'}/>
+        <TextField
+          id="standard-textarea"
+          label="message"
+          multiline
+          variant="standard"
+          margin={'dense'}
+          rows={8}
+        />
+        <Button variant="text" endIcon={<Send/>}>Send</Button>
+      </form>
+    </ThemeProvider>
+  );
 };
 
 export default Form;
