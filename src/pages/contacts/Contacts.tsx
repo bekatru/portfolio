a124e5862f8dc087a55c90f6fd1bc6fddfdc8ef3
bekatru <teamusicband@gmail.com>
2023-12-12 19:02:45 +0600

    refactor Contacts page layout

--- src/pages/contacts/Contacts.tsx
+++ src/pages/contacts/Contacts.tsx
@@ -1,11 +1,46 @@
 import {FC} from 'react';
+import {Email, LinkedIn, Telegram} from '@mui/icons-material';
+import {ThemeProvider} from '@mui/material';
+import {createTheme} from '@mui/material/styles';
 import {Form} from '../../components';
 
+import './Contacts.css';
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const CONTACT_LIST = [
+  {
+    Icon: Email,
+    link: 'mailto:hithisisbeka@gmail.com'
+  },
+  {
+    Icon: LinkedIn,
+    link: 'https://www.linkedin.com/in/bekzat-amatakhunov/'
+  },
+  {
+    Icon: Telegram,
+    link: 'https://t.me/tuemorrow'
+  },
+]
 
 const Contacts: FC = () => {
 
   return (
-    <Form />
+    <ThemeProvider theme={darkTheme}>
+      <div className={'contacts-content'}>
+        <div className={'contacts-link-icons'}>
+          {CONTACT_LIST.map(({Icon, link}) =>
+            <Icon key={link} fontSize={'large'} color={'action'} onClick={() => window.open(link)}/>
+          )}
+        </div>
+        <Form/>
+      </div>
+    </ThemeProvider>
   );
 };
 
